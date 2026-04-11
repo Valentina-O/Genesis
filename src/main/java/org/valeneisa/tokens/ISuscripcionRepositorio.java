b6a2242b638cdfc494ea1a1c20d03f1f61aea465
milerickhenaor-alt <milerick.henaor@autonomae.edu.co>
2026-04-11 15:40:41 -0500

    feat(database): add repositorios JPA para todas las entidades

---
+++ src/main/java/org/valeneisa/tokens/ISuscripcionRepositorio.java
@@ -0,0 +1,6 @@
+package org.valeneisa.tokens;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ISuscripcionRepositorio extends JpaRepository<Suscripcion, Long> {
+}
