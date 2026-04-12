81eef8ca029eca9ea9cd13888ee9a551694084fb
milerickhenaor-alt <milerick.henaor@autonoma.edu.co>
2026-04-11 20:23:51 -0500

    Revert "feat: implementación CRUD de PlanDevelop"

--- src/main/java/org/valeneisa/Core/IOperacion.java
+++
@@ -1,8 +0,0 @@
-package org.valeneisa.Core;
-
-public interface IOperacion <T_REQ, T_RES>{
-    String obtenerCodigoOp();
-    int obtenerCostoBase();
-    T_RES ejecutar(T_REQ solicitud);
-
-}
--- src/main/java/org/valeneisa/tokens/IPlanRepositorio.java
+++
@@ -1,6 +0,0 @@
-package org.valeneisa.tokens;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPlanRepositorio extends JpaRepository<Plan, Long> {
-}
